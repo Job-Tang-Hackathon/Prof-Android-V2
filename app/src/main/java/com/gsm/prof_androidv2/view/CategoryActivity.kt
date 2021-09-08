10bdcf03d6a21996f6e02c347f37ab3692430304
ParkSangSun <appleasz1028102@gmail.com>
2021-09-08 11:10:49 +0900

    [CREATE] CategoryActivity

---
+++ app/src/main/java/com/gsm/prof_androidv2/view/CategoryActivity.kt
@@ -0,0 +1,11 @@
+package com.gsm.prof_androidv2.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
