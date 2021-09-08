059ed49e1bf94e02a763ec53703e57d1a9326413
ParkSangSun <appleasz1028102@gmail.com>
2021-09-08 11:44:33 +0900

    [ADD] TitleBar삭제, style 색 변경

--- app/src/main/java/com/gsm/prof_androidv2/view/CategoryActivity.kt
+++ app/src/main/java/com/gsm/prof_androidv2/view/CategoryActivity.kt
@@ -2,6 +2,7 @@ package com.gsm.prof_androidv2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gsm.prof_androidv2.R
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
