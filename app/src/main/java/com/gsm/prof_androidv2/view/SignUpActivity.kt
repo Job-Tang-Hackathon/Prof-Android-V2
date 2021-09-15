9e080d5040203b5763c31a08fee94ee1091ddab0
ParkSangSun <appleasz1028102@gmail.com>
2021-09-15 14:59:08 +0900

    [UPDATE] xml수정 및 activity 연결

--- app/src/main/java/com/gsm/prof_androidv2/view/SignUpActivity.kt
+++ app/src/main/java/com/gsm/prof_androidv2/view/SignUpActivity.kt
@@ -34,25 +34,38 @@ class SignUpActivity : AppCompatActivity() {
         signUpViewModel.signUpResponse.observe(this, Observer {
             binding.loading.visibility = View.GONE
             when (it) {
-                1 -> Toast.makeText(this, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                1 -> {
+                    Toast.makeText(this, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+
                 0, 2 -> Toast.makeText(this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show()
             }
         })
     }
 
     fun createAccount(view: View) {
-        if (!TextUtils.isEmpty(binding.email.text.toString()) && !TextUtils.isEmpty(binding.password.text.toString()) && !TextUtils.isEmpty(binding.passwordCheck.text.toString())) {
+        if (!TextUtils.isEmpty(binding.email.text.toString()) && !TextUtils.isEmpty(binding.password.text.toString()) && !TextUtils.isEmpty(
+                binding.passwordCheck.text.toString()
+            )
+        ) {
             if (useRegex(binding.email.text.toString())) {
-                if(binding.password.text.toString().length >=6){
-                    if (checkPasswordSame(binding.password.text.toString(), binding.passwordCheck.text.toString())){
+                if (binding.password.text.toString().length >= 6) {
+                    if (checkPasswordSame(
+                            binding.password.text.toString(),
+                            binding.passwordCheck.text.toString()
+                        )
+                    ) {
                         binding.loading.visibility = View.VISIBLE
-                        signUpViewModel.signUp(binding.email.text.toString(), binding.password.text.toString())
-                    }
-                    else Toast.makeText(this, "재확인 비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                        signUpViewModel.signUp(
+                            binding.email.text.toString(),
+                            binding.password.text.toString()
+                        )
+                    } else Toast.makeText(this, "재확인 비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
 
-                }else Toast.makeText(this, "비밀번호를 6자 이상 작성해 주세요", Toast.LENGTH_SHORT).show()
+                } else Toast.makeText(this, "비밀번호를 6자 이상 작성해 주세요", Toast.LENGTH_SHORT).show()
 
-            }else Toast.makeText(this, "학교 계정을 사용하세요", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this, "학교 계정을 사용하세요", Toast.LENGTH_SHORT).show()
 
         } else Toast.makeText(this, "필수 항목을 입력해 주세요", Toast.LENGTH_SHORT).show()
 
@@ -65,10 +78,14 @@ class SignUpActivity : AppCompatActivity() {
     //이메일 정규식 필터
     fun useRegex(input: String): Boolean {
         val regex = Regex(
-            pattern = "^[a-zA-Z][0-9][0-9][0-9][0-9][0-9]+@[a-zA-Z]sm\\.hs\\.kr\$",
+            pattern = "^[a-zA-Z][0-9][0-9][0-9][0-9][0-9]+@gsm\\.hs\\.kr\$",
             options = setOf(RegexOption.IGNORE_CASE)
         )
         return regex.matches(input)
+    }
+
+    fun backBtnClick(view: View) {
+        finish()
     }
 
 }
