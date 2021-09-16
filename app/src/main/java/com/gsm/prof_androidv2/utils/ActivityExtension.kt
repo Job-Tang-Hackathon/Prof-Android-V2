0ab914cf78d6fd53e1aceb04ae04b7a3916b8068
KyungMin <2004ckck@naver.com>
2021-09-16 20:09:46 +0900

    add ActivityExtension.kt

---
+++ app/src/main/java/com/gsm/prof_androidv2/utils/ActivityExtension.kt
@@ -0,0 +1,18 @@
+package com.gsm.prof_androidv2.utils
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+//RecyclerView Vertical
+fun RecyclerView.showVertical(context: Context){
+    this.layoutManager =
+        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+}
+
+fun AppCompatActivity.startActivityWithFinish(context: Context, activity: Class<*>) {
+    startActivity(Intent(context, activity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    this.finish()
+}
