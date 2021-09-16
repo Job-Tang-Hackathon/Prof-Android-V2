f7601d8868e62f0202a284aa52b2ea8c428d4ffc
ParkSangSun <appleasz1028102@gmail.com>
2021-09-16 23:33:28 +0900

    [REFACTORING] AllPostFragment.kt

--- app/src/main/java/com/gsm/prof_androidv2/view/AllPostFragment.kt
+++ app/src/main/java/com/gsm/prof_androidv2/view/AllPostFragment.kt
@@ -18,59 +18,61 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AllPostFragment : Fragment() {
-    lateinit var binding : FragmentAllPostBinding
+    lateinit var binding: FragmentAllPostBinding
     private val mainViewModel by activityViewModels<MainViewModel>()
-    private val TAG = "AllPost"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_all_post,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_all_post, container, false)
         observeViewModel()
 
         return binding.root
     }
 
-    private fun initRecyclerView(){
+
+    private fun initRecyclerView() {
         binding.allPostRecyclerView.showVertical(requireContext())
         binding.allPostRecyclerView.adapter = MainRecyclerAdapter(mainViewModel)
     }
 
-    private fun observeViewModel(){
+
+    private fun observeViewModel() {
         mainViewModel.getPostResponse.observe(requireActivity(), androidx.lifecycle.Observer {
             //게시물 있음
-            if (it !=null){
+            if (it != null) {
                 for (document in it.result) {
-                    Log.d("로그","옵져버 : ${document.data}")
                     initRecyclerView()
                     mainViewModel.setGetAllPostNull(false)
                 }
-            }else{
+            } else {
                 //게시물 없음
-                Log.d("로그","옵져버 널")
                 mainViewModel.setGetAllPostNull(true)
             }
-
         })
 
-
         mainViewModel.message.observe(requireActivity(), Observer {
-            when(it){
-                "blank" -> Toast.makeText(requireContext(),"검색된 게시물이 없습니다", Toast.LENGTH_SHORT).show()
+            when (it) {
+                "blank" -> Toast.makeText(requireContext(), "검색된 게시물이 없습니다", Toast.LENGTH_SHORT)
+                    .show()
+                "error" -> Toast.makeText(requireContext(), "서버에 오류가 발생했습니다", Toast.LENGTH_SHORT)
+                    .show()
             }
         })
 
         mainViewModel.getAllPostNull.observe(requireActivity(), androidx.lifecycle.Observer {
-            if(it == true){
+            if (it == true) {
                 binding.allPostRecyclerView.visibility = View.GONE
                 binding.notFound.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.allPostRecyclerView.visibility = View.VISIBLE
                 binding.notFound.visibility = View.GONE
             }
