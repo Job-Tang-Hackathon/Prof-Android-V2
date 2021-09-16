51bc60d714dd889fcaa55272f4036d49a33b9635
KyungMin <2004ckck@naver.com>
2021-09-16 11:47:31 +0900

    add dto Parcelize

--- app/src/main/java/com/gsm/prof_androidv2/model/dto/GetCategoryPostDto.kt
+++ app/src/main/java/com/gsm/prof_androidv2/model/dto/GetCategoryPostDto.kt
@@ -1,5 +1,9 @@
 package com.gsm.prof_androidv2.model.dto
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class GetCategoryPostDto(
     val fullExplanation: String = "",
     val oneLineExplanation: String = "",
@@ -8,4 +12,4 @@ data class GetCategoryPostDto(
     val state: String ="",
     val tag: String ="",
     val title: String =""
-)
+): Parcelable
