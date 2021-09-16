b9ef99a5e0b5413118c61940584c66de6245ec20
KyungMin <2004ckck@naver.com>
2021-09-16 20:06:49 +0900

    add splash

---
+++ app/src/main/java/com/gsm/prof_androidv2/view/SplashActivity.kt
@@ -0,0 +1,47 @@
+
+package com.gsm.prof_androidv2.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.gsm.prof_androidv2.R
+
+class SplashActivity  : AppCompatActivity() {
+    lateinit var uid :String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                checkUserUid()
+
+            },
+            1500
+        )
+
+
+    }
+
+    private fun checkUserUid(){
+        uid = FirebaseAuth.getInstance().uid.toString()
+        Log.d("로그","uid : $uid")
+        if(uid != "null"){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
