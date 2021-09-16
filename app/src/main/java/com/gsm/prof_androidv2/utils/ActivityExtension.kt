e01c07e68faf74c6fe89ea9cc7c13dd9224a6e55
ParkSangSun <appleasz1028102@gmail.com>
2021-09-16 15:45:39 +0900

    [ADD] 자동로그인

--- app/src/main/java/com/gsm/prof_androidv2/utils/ActivityExtension.kt
+++ app/src/main/java/com/gsm/prof_androidv2/utils/ActivityExtension.kt
@@ -1,6 +1,8 @@
 package com.gsm.prof_androidv2.utils
 
 import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,4 +10,9 @@ import androidx.recyclerview.widget.RecyclerView
 fun RecyclerView.showVertical(context: Context){
     this.layoutManager =
         LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+}
+
+fun AppCompatActivity.startActivityWithFinish(context: Context, activity: Class<*>) {
+    startActivity(Intent(context, activity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    this.finish()
 }
